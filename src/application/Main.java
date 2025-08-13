8e5521ed0d3ce05659a8241f5c08b9c2c5c5ac35
Rickzx7 <ricardoaraujocandido516@gmail.com>
2025-08-13 15:52:19 -0300

    Creating seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.SQLOutput;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(1, "Rick", "rick@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
