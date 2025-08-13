ac359217631e00cce47e1aab3a34ab496cae4e5a
Rickzx7 <ricardoaraujocandido516@gmail.com>
2025-08-13 16:10:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +14,8 @@ public class Main {
 
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         Seller seller = new Seller(1, "Rick", "rick@gmail.com", new Date(), 3000.0, department);
         System.out.println(seller);
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+
+    @Override
+    public void insertSeller(Seller seller) {
+
+    }
+
+    @Override
+    public void updateSeller(Seller seller) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
